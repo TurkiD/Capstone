f7e519a1e902b0c948b672e66aeae927d9052258
Turki Dhawi <T.Dhawi@outlook.com>
2025-01-13 11:43:57 +0300

    added post form page

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+var formRouter = require('./routes/post');
 
 var app = express();
 const port = 3000;
@@ -23,6 +24,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/postForm', formRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
